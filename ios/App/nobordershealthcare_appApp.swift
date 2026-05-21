db97f9684dd002f1949d85a2d5b88c5895353960
maxib0ss <valeriy.indyk@gmail.com>
2026-05-21 16:16:34 +0200

    feat: add Xcode SwiftUI app shell

---
+++ ios/App/nobordershealthcare_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  nobordershealthcare_appApp.swift
+//  nobordershealthcare-app
+//
+//  Created by Valeriy Indyk on 21.05.26.
+//
+
+import SwiftUI
+
+@main
+struct nobordershealthcare_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
